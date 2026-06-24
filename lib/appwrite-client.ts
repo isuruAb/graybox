13b0ee608823e6bfa6ecb6e3c97848f46eb533c7
isuruAb <vpisabeywardana@gmail.com>
2026-06-24 23:02:06 +0800

    add example environment variables file

--- lib/appwrite-client.ts
+++ lib/appwrite-client.ts
@@ -2,6 +2,6 @@ import { Client } from "appwrite";
 
 export const client = new Client()
   .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!);
+  .setProject(process.env.PROJECT_ID!);
 
-export const ALERTS_CHANNEL = `databases.${process.env.NEXT_PUBLIC_DATABASE_ID}.collections.${process.env.NEXT_PUBLIC_ALERT_COLLECTION_ID}.documents`;
+export const ALERTS_CHANNEL = `databases.${process.env.DATABASE_ID}.collections.${process.env.ALERT_COLLECTION_ID}.documents`;
